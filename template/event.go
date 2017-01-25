3fa2e2313782b2f787ca14e5dff443350592e93d
Adrià Cidre <adria.cidre@gmail.com>
2017-01-25 12:11:07 +0100

    Share common methods

--- template/event.go
+++ template/event.go
@@ -5,15 +5,14 @@
 package template
 
 import (
-	"encoding/json"
 	"errors"
-	"log"
 
 	"github.com/ernestio/ernestazure"
 )
 
 // Event stores the template data
 type Event struct {
+	ernestazure.Base
 	ErrorMessage string `json:"error,omitempty"`
 	Subject      string `json:"-"`
 	Body         []byte `json:"-"`
@@ -25,43 +24,6 @@ func New(subject string, body []byte, cryptoKey string) ernestazure.Event {
 	n := Event{Subject: subject, Body: body, CryptoKey: cryptoKey}
 
 	return &n
-}
-
-// GetBody : Gets the body for this event
-func (ev *Event) GetBody() []byte {
-	var err error
-	if ev.Body, err = json.Marshal(ev); err != nil {
-		log.Println(err.Error())
-	}
-	return ev.Body
-}
-
-// GetSubject : Gets the subject for this event
-func (ev *Event) GetSubject() string {
-	return ev.Subject
-}
-
-// Process : starts processing the current message
-func (ev *Event) Process() (err error) {
-	if err := json.Unmarshal(ev.Body, &ev); err != nil {
-		ev.Error(err)
-		return err
-	}
-
-	if err := ev.Validate(); err != nil {
-		ev.Error(err)
-		return err
-	}
-
-	return nil
-}
-
-// Error : Will respond the current event with an error
-func (ev *Event) Error(err error) {
-	log.Printf("Error: %s", err.Error())
-	ev.ErrorMessage = err.Error()
-
-	ev.Body, err = json.Marshal(ev)
 }
 
 // Validate checks if all criteria are met
